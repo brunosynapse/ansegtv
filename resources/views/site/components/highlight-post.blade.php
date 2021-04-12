3035b21230adb439984713fa49ff77bb79ab9e8a
alessondejesus <asn9006@hotmail.com>
2021-04-12 10:06:34 -0500

    corrigindo responsividade de publicacoes em destaques na home site

--- resources/views/site/components/highlight-post.blade.php
+++ resources/views/site/components/highlight-post.blade.php
@@ -1,21 +1,22 @@
 <div class="row">
-    <div class="col-12 col-md-8 main-news">
-        @if($highlight_1 = $posts::active()->highlight(1)->first())
-            <a href="{{route('site.posts.show', $highlight_1->path)}}">
-                <div class="news-container" style="background-image: url('{{asset("storage/".$highlight_1->image)}}');">
-                    <div class="news-overlay">
-                        <span class="font-lato date-style text-white">{{$highlight_1->formatted_date}}</span>
-                        <h5 class="my-3 news-info text-white">{{$highlight_1->formatted_category_name}}</h5>
-                        <h4 class="font-lato title-style text-white">{{$highlight_1->title}}</h4>
+
+    @if($highlight_1 = $posts::active()->highlight(1)->first())
+        <div class="col-12 @if($posts::active()->highlight(2)->count() or $posts::active()->highlight(3)->count()) col-md-8 @else  col-md-12 @endif main-news">
+                <a href="{{route('site.posts.show', $highlight_1->path)}}">
+                    <div class="news-container" style="background-image: url('{{asset("storage/".$highlight_1->image)}}');">
+                        <div class="news-overlay">
+                            <span class="font-lato date-style text-white">{{$highlight_1->formatted_date}}</span>
+                            <h5 class="my-3 news-info text-white">{{$highlight_1->formatted_category_name}}</h5>
+                            <h4 class="font-lato title-style text-white">{{$highlight_1->title}}</h4>
+                        </div>
                     </div>
-                </div>
-            </a>
-        @endif
-    </div>
+                </a>
+        </div>
+    @endif
 
     <div class="col-12 col-md-4 sidebar-news">
-        <div class="col-12 destaque-2">
-            @if($highlight_2 = $posts::active()->highlight(2)->first())
+        @if($highlight_2 = $posts::active()->highlight(2)->first())
+            <div class="col-12 destaque-2">
                 <a href="{{route('site.posts.show', $highlight_2->path)}}">
                     <div class="sidebar-news-container" style="background-image: url('{{asset("storage/".$highlight_2->image)}}');">
                         <div class="sidebar-news-overlay">
@@ -24,11 +25,11 @@
                         </div>
                     </div>
                 </a>
-            @endif
-        </div>
+            </div>
+        @endif
 
-        <div class="col-12 destaque-3">
-            @if($highlight_3 = $posts::active()->highlight(3)->first())
+        @if($highlight_3 = $posts::active()->highlight(3)->first())
+            <div class="col-12 destaque-3">
                 <a href="{{route('site.posts.show', $highlight_3->path)}}">
                     <div class="sidebar-news-container" style="background-image: url('{{asset("storage/".$highlight_3->image)}}');">
                         <div class="sidebar-news-overlay">
@@ -37,7 +38,7 @@
                         </div>
                     </div>
                 </a>
-            @endif
-        </div>
+            </div>
+        @endif
     </div>
 </div>
