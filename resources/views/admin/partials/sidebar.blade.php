01bcd81ad0d1ab18c9ecc2a160de89dff851f732
alessondejesus <asn9006@hotmail.com>
2021-04-26 13:03:11 -0500

    feat: active nas abas ativas do sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -7,15 +7,15 @@
   </div>
   <ul class="sidebar-menu">
     <li class="menu-header">Menu</li>
-      <li class="dropdown {{ request()->is('posts') ? 'active' : '' }}">
+      <li class="dropdown {{ request()->is('admin/posts*') ? 'active' : '' }}">
           <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-copy"></i> <span>Notícias</span></a>
           <ul class="dropdown-menu" style="display: none;">
-              <li><a class="active" href="{{ route('admin.posts.index') }}">Todas as Notícias</a></li>
+              <li><a class="nav-link" href="{{ route('admin.posts.index') }}">Todas as Notícias</a></li>
               <li><a class="nav-link" href="{{ route('admin.posts.create') }}">Criar Nova Notícia</a></li>
           </ul>
       </li>
-      <li class="{{ request()->is('categories') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.categories.index') }}"><i class="fas fa-list-alt"></i> <span>Categorias</span></a></li>
+      <li class="{{ request()->is('admin/categories*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.categories.index') }}"><i class="fas fa-list-alt"></i> <span>Categorias</span></a></li>
       <li class="menu-header">Mais Opções</li>
-      <li class="{{ request()->is('users') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i> <span>Usuários</span></a></li>
+      <li class="{{ request()->is('admin/users*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i> <span>Usuários</span></a></li>
   </ul>
 </aside>
